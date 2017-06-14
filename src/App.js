d32c758e4f3eb5cf122867dfd4f42f8d9f2cc782
Christian Broberg <cb@webhouse.dk>
2017-06-14 20:52:30 +0200

    Grrrr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 // import { Layout, Wrapper, Spacer } from './components/Styles'
-import { Layout, Wrapper, Spacer } from 'components/Styles'
+import { Layout, Wrapper, Spacer } from './components/Styles'
 /*eslint-disable*/
 import IconTest from './IconTest'
 import { ButtonTest, ButtonTest2 } from './ButtonTest'
